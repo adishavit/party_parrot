f4c2f2317c992d9f0168d68c06a0cb16c3df5ccb
Adi Shavit <adishavit@gmail.com>
2017-06-10 14:45:01 +0300

    Adds support for video files.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
 
 using namespace std;
 using namespace cv;
@@ -12,33 +13,42 @@ int main(int argc, char* argv[])
       return EXIT_FAILURE;
    }
 
-   auto img0 = imread(argv[1]);
-   assert( 3 == img0.channels());
-   assert( img0.isContinuous());
-
-   cv::Mat3b hsv0, hsv1, res;
-   cv::Mat1b gray;
-   cvtColor(img0, hsv0, CV_BGR2HSV_FULL);
-   hsv1.create(hsv0.size());
-
-   int hdelta = 0;
-   while (27 != waitKey(10))
+   VideoCapture vidcap(argv[1]);
+   if (!vidcap.isOpened())
    {
-       for (int r = 0; r < hsv0.rows; ++r)
-           for (int c = 0; c < hsv0.cols; ++c)
-               hsv1.at<Vec3b>(r, c) = cv::Vec3b((hsv0.at<Vec3b>(r, c)[0] + hdelta) % 255,
-                                                 hsv0.at<Vec3b>(r, c)[1],
-                                                 hsv0.at<Vec3b>(r, c)[2]);
-
-       cvtColor(hsv1, res, CV_HSV2BGR_FULL);
-       cvtColor(res, gray, CV_BGR2GRAY);
-       imshow("Party", res);
-       imshow("Gray", gray);
-
-       hdelta += 5;
-
+       std::cerr << "Could not open media '" << argv[1] << "'." << std::endl;
+       return EXIT_FAILURE;
    }
 
+   cv::Mat3b img, hsv0, hsv1, res;
+   cv::Mat1b gray;
+
+   int hdelta = 0;
+   int c = 0;
+   while (27 != c)
+   {
+       while (27 != (c = waitKey(1.5*1000.f/vidcap.get(CAP_PROP_FPS))) && (vidcap >> img, !img.empty()))
+       {
+           cvtColor(img, hsv0, CV_BGR2HSV_FULL);
+           hsv1.create(hsv0.size());
+
+           for (int r = 0; r < hsv0.rows; ++r)
+               for (int c = 0; c < hsv0.cols; ++c)
+                   hsv1.at<Vec3b>(r, c) = cv::Vec3b((hsv0.at<Vec3b>(r, c)[0] + hdelta) % 255,
+                       hsv0.at<Vec3b>(r, c)[1],
+                       hsv0.at<Vec3b>(r, c)[2]);
+
+           cvtColor(hsv1, res, CV_HSV2BGR_FULL);
+           cvtColor(res, gray, CV_BGR2GRAY);
+           imshow("Party", res);
+           imshow("Gray", gray);
+
+           hdelta += 10;
+
+       }
+       //vidcap.set(CAP_PROP_POS_FRAMES, 0);
+       vidcap.open(argv[1]);
+   }
 
    waitKey();
 	return EXIT_SUCCESS;
