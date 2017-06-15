5dd8f16f5ae6e687c124f71d37e6d0a56cac34b7
Adi Shavit <adishavit@gmail.com>
2017-06-15 17:32:18 +0300

    Simplifies color cycling code. Reduces memory footprint.

--- color_cycle.cpp
+++ color_cycle.cpp
@@ -1,35 +1,33 @@
 #include <opencv2/imgproc.hpp>
-
 #include "color_cycle.h"
 
-
-cv::Mat3b hsv_in, hsv_out;
+cv::Mat3b hsv;
 int accHOffset = 0;
-
-using namespace cv;
 
 void rotate_hue(cv::Mat3b const& img, cv::Mat3b& result_img, int hsteps)
 {
    assert(result_img.size() == img.size() && result_img.type() == img.type());
-   hsv_in.create(img.size());
-   hsv_out.create(hsv_in.size());
 
-   cvtColor(img, hsv_in, CV_BGR2HSV_FULL);
+   // re-allocate global temp storage if needed
+   hsv.create(img.size());
+   
+   // convert to HSV
+   cv::cvtColor(img, hsv, CV_BGR2HSV_FULL);
 
-   for (int r = 0; r < hsv_in.rows; ++r)
-      for (int c = 0; c < hsv_in.cols; ++c)
-         hsv_out.at<Vec3b>(r, c) = cv::Vec3b((hsv_in.at<Vec3b>(r, c)[0] + accHOffset) % 255,
-            hsv_in.at<Vec3b>(r, c)[1],
-            hsv_in.at<Vec3b>(r, c)[2]);
+   for (int r = 0; r < hsv.rows; ++r)
+      for (int c = 0; c < hsv.cols; ++c)
+         hsv.at<cv::Vec3b>(r, c)[0] = (hsv.at<cv::Vec3b>(r, c)[0] + accHOffset) % 255; // cycle H
 
-   cvtColor(hsv_out, result_img, CV_HSV2BGR_FULL);
+   // convert back to BGR
+   cv::cvtColor(hsv, result_img, CV_HSV2BGR_FULL);
+   
+   // update cycle offset
    accHOffset += hsteps;
 }
 
-
 void clear_all()
 {
-   hsv_in.release();
-   hsv_out.release();
+   // release global's memory
+   hsv.release();
    accHOffset = 0;
 }
